e68f2e485b44076ed8bf9848749dc7148569748a
Duvan Arley Botero Rivillas <duvan.botero@innovacionypagos.com.pa>
2025-08-29 12:07:27 -0500

    feat: add authentication service configuration and update WebClientAdapterConfig to use dynamic base URL

--- applications/app-service/src/main/java/co/com/crediya/solicitudes/config/WebClientAdapterConfig.java
+++ applications/app-service/src/main/java/co/com/crediya/solicitudes/config/WebClientAdapterConfig.java
@@ -2,6 +2,7 @@ package co.com.crediya.solicitudes.config;
 
 import co.com.crediya.solicitudes.model.client.gateways.ClientValidationGateway;
 import co.com.crediya.solicitudes.webclient.AuthServiceClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,11 +10,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientAdapterConfig {
 
+    @Value("${auth-service.base-url}")
+    private String authServiceBaseUrl;
+
     @Bean
     public ClientValidationGateway clientValidationGateway() {
         WebClient webClient = WebClient.builder()
                 .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(1024 * 1024))
                 .build();
-        return new AuthServiceClient(webClient, "http://localhost:8080");
+        return new AuthServiceClient(webClient, authServiceBaseUrl);
     }
 }
