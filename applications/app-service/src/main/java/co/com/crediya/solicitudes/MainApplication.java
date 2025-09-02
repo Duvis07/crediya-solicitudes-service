17ac7e28a5ddddfd9a011688703d97ef24876700
Duvan Arley Botero Rivillas <duvan.botero@innovacionypagos.com.pa>
2025-09-01 20:18:04 -0500

    feat: integrate dotenv for environment variable management and update configuration files

--- applications/app-service/src/main/java/co/com/crediya/solicitudes/MainApplication.java
+++ applications/app-service/src/main/java/co/com/crediya/solicitudes/MainApplication.java
@@ -1,13 +1,34 @@
 package co.com.crediya.solicitudes;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication app = new SpringApplication(MainApplication.class);
+        app.addInitializers(applicationContext -> {
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            Map<String, Object> dotenvMap = dotenv.entries()
+                .stream()
+                .collect(Collectors.toMap(
+                        DotenvEntry::getKey,
+                        DotenvEntry::getValue
+                ));
+            environment.getPropertySources().addLast(
+                new MapPropertySource("dotenv", dotenvMap)
+            );
+        });
+        app.run(args);
     }
 }
